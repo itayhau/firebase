027da9b40d7b4b04e220324722f19b25e430a1b1
itayhau <itayhau@gmail.com>
2019-04-30 19:22:36 +0300

    Create advanced.cs

---
+++ advanced.cs
@@ -0,0 +1,141 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using FireSharp.Config;
+using FireSharp.Interfaces;
+using FireSharp.Response;
+
+namespace FBConsole
+{
+    // --------------------------------------------------
+    // requires FireSharp 2+
+    // --------------------------------------------------
+    
+    
+    class Program
+    {
+
+        static IFirebaseClient firebaseClient;
+
+        static IFirebaseConfig config = new FirebaseConfig
+        {
+            AuthSecret = "yOn0WGMYZ0NahbuiTBlXrAfAC1HYgGSNMricEi7v",
+            BasePath = "https://demo1-25b64.firebaseio.com/"
+        };
+
+        static private void Push()
+        {
+            var data = new Data
+            {
+                ID = "1",
+                Name = "AUSTIN"
+            };
+
+            PushResponse response = firebaseClient.Push("Customer/1", data);
+            Data result = response.ResultAs<Data>();
+
+            Console.WriteLine("Data pushed " + data.ID);
+        }
+
+        static private void Update()
+        {
+            var data = new Data
+            {
+                ID = "1",
+                Name = "AUSTIN !"
+            };
+
+            FirebaseResponse response = firebaseClient.Update("Customer/1", data);
+            Data result = response.ResultAs<Data>();
+
+            Console.WriteLine("Data updated " + data.ID);
+        }
+
+
+        static private void Delete()
+        {
+            var data = new Data
+            {
+                ID = "1",
+                Name = "AUSTIN"
+            };
+
+            FirebaseResponse response = firebaseClient.Delete("Customer/1");
+
+            Console.WriteLine("Data deleted " + data.ID);
+        }
+
+        static private void Add()
+        {
+            var data = new Data
+            {
+                ID = "1",
+                Name = "AUSTIN"
+            };
+
+            SetResponse response = firebaseClient.Set("Customer/1", data);
+            Data result = response.ResultAs<Data>();
+
+            Console.WriteLine("Data inserted " + data.ID);
+        }
+
+        static private void GetAll()
+        {
+
+            FirebaseResponse response = firebaseClient.Get("Customer");
+            var result = response.ResultAs<List<Data>>();
+            result.RemoveAt(0);
+            result.ForEach(d => Console.WriteLine(Newtonsoft.Json.JsonConvert.SerializeObject(d)));
+        }
+
+        static private void GetOne()
+        {
+            FirebaseResponse response = firebaseClient.Get("Customer/1");
+            
+            Console.WriteLine("111111111");
+            Data result = response.ResultAs<Data>();
+
+            Console.WriteLine(result.ID);
+            Console.WriteLine(result.Name);
+        }
+
+        static async void ListenToAdd()
+        {
+            // requires FireSharp 2+
+            await firebaseClient.ListenAsync("Customer", (sender, args, z) => { Console.WriteLine($"Data changed at Path {args.Path} Data : {args.Data}"); });
+        }
+
+        static void Main(string[] args)
+        {
+            firebaseClient = new FireSharp.FirebaseClient(config);
+            if (firebaseClient != null)
+            {
+                Console.WriteLine("Connection Succeeded!");
+            }
+            try
+            {
+                Add();
+                GetOne();
+                Push();
+                Update();
+                GetAll();
+                ListenToAdd();
+                //Delete();
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+            Console.WriteLine("Done");
+
+            while (true)
+            {
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
